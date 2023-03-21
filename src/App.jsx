93a0d514ceed1cfa575dee05640d9ebed5557012
rohit5683 <killerrohitrocks007@gmail.com>
2023-03-22 00:52:38 +0530

    We learn about React Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import './style.scss';
 import Board from './components/Board';
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  const onBtnClick = () => {
+    console.log('Hello');
+
+    setCounter(currentCounter => {
+      return currentCounter + 1;
+    });
+  };
+
   return (
     <div className="app">
       <Board />
+      <button onClick={onBtnClick}>Please Click me</button>
+      <div>{counter}</div>
     </div>
   );
 }
