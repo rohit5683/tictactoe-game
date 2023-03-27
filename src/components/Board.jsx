4e5d0a61f64fd5cb407ad31d1c03be42dc4d972c
rohit5683 <killerrohitrocks007@gmail.com>
2023-03-28 01:40:26 +0530

    Lifted up the state to app components. Show status message to the user, calculate winner

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,26 +1,6 @@
 import Square from './Square';
-import { useState } from 'react';
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
-
-  const handleSquareClick = clickedPosition => {
-    if (squares[clickedPosition]) {
-      return;
-    }
-
-    setSquares(currentSquares => {
-      return currentSquares.map((squareValue, position) => {
-        if (clickedPosition == position) {
-          return isXNext ? 'X' : 'O';
-        }
-        return squareValue;
-      });
-    });
-    setIsXNext(currentIsXNext => !currentIsXNext);
-  };
-
+const Board = ({ squares, handleSquareClick }) => {
   const renderSquare = position => {
     return (
       <Square
@@ -29,6 +9,7 @@ const Board = () => {
       />
     );
   };
+
   return (
     <div className="board">
       <div className="board-row">
